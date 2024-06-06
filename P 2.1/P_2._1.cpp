d6baf74ce02e48adf6de24987636722af93952f1
onix1t <ioann.tsvetkov@gmail.com>
2024-06-07 01:42:16 +0300

    1

---
+++ P 2.1/P_2._1.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void ex1()
+{
+    cout << "Задание №1\n";
+    
+    string text;
+    getline (cin, text);
+    const string separators{ " ,;:.\"!?'*\n" };
+    unsigned count{};
+    size_t start { text.find_first_not_of(separators) };
+
+    while (start != string::npos)
+    {
+        count++;
+        size_t end = text.find_first_of(separators, start + 1);
+        if (end == string::npos) 
+        {
+            end = text.length();
+        }
+        start = text.find_first_not_of(separators, end + 1);
+    }
+    
+    cout << "Ответ: Текст содержит " << count << " сл.\r\n";
+}
+
+void ex2()
+{
+    cout << "Задание №2\n";
+
+    string people[] {"Tom", "Alice", "Sam", "Bob", "Kate"};
+    cout << "Изначальный список имён: Tom, Alice, Sam, Bob, Kate\n";
+    cout << "Ответ: ";
+    // сортируем по возрастанию
+    bool sorted {};
+    do
+    {
+        sorted = true;      // остается true, если строки отсортированы
+        string temp;   // переменная для обмена значениями
+        for (unsigned i {1}; i < size(people); i++)
+        {
+            // если предыдущая строка больше последующей
+            if (people[i-1] > people[i])
+            { 
+                // обмениваем значения
+                temp = people[i];
+                people[i] = people[i-1];
+                people[i-1] = temp;
+                sorted = false;
+            }
+        }
+    } while (!sorted);
+    // вывод строк на консоль
+    for (const auto person: people) 
+    {
+        cout << person << ", ";
+    }
+    
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Практика №1\r\n";
+    ex1();
+    ex2();
+    
+    return 0;
+}
